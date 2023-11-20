1fb800e8eba8392b5b447065c803ee32a2740cad
harriet221 <potter19@dongguk.edu>
2023-11-20 11:04:09 +0900

    ãCREATE project

---
+++ src/main/java/com/ll/mission_jump_to_sb/MissionJumpToSbApplication.java
@@ -0,0 +1,13 @@
+package com.ll.mission_jump_to_sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MissionJumpToSbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MissionJumpToSbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/mission_jump_to_sb/MissionJumpToSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.mission_jump_to_sb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MissionJumpToSbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
