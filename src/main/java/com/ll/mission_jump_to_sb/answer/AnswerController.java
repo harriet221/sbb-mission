495d4de789c082f0ddf84457758a3586409f811b
harriet221 <potter19@dongguk.edu>
2023-11-23 17:32:35 +0900

    chapter 2-11 답변 등록 1

---
+++ src/main/java/com/ll/mission_jump_to_sb/answer/AnswerController.java
@@ -0,0 +1,26 @@
+package com.ll.mission_jump_to_sb.answer;
+
+import com.ll.mission_jump_to_sb.question.Question;
+import com.ll.mission_jump_to_sb.question.QuestionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RequestMapping("/answer")
+@RequiredArgsConstructor
+@Controller
+public class AnswerController {
+
+    private final QuestionService questionService;
+
+    @PostMapping("/create/{id}")
+    public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam String content) {
+        Question question = this.questionService.getQuestion(id);
+        // TODO: 답변을 저장한다. 이건 뭐야??? 신기하군...
+        return String.format("redirect:/question/detail/%s", id);
+    }
+}
