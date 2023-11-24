d4ed41a55bcf4fb6de94070c69817180ba8664c1
harriet221 <potter19@dongguk.edu>
2023-11-25 00:46:52 +0900

    chapter 2-15 질문등록과 폼 4

--- src/main/java/com/ll/mission_jump_to_sb/question/QuestionController.java
+++ src/main/java/com/ll/mission_jump_to_sb/question/QuestionController.java
@@ -1,5 +1,6 @@
 package com.ll.mission_jump_to_sb.question;
 
+import com.ll.mission_jump_to_sb.answer.AnswerForm;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -24,7 +25,7 @@ public class QuestionController {
     }
 
     @GetMapping(value = "/detail/{id}")
-    public String detail(Model model, @PathVariable("id") Integer id) {
+    public String detail(Model model, @PathVariable("id") Integer id, AnswerForm answerForm) {
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question", question);
         return "question_detail";
