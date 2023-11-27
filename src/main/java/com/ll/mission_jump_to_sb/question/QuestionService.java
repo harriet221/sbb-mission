d3d4821606ba0928ef135fa5cb6372603819a2d3
harriet221 <potter19@dongguk.edu>
2023-11-27 17:41:40 +0900

    chapter 3-02 페이징 2

--- src/main/java/com/ll/mission_jump_to_sb/question/QuestionService.java
+++ src/main/java/com/ll/mission_jump_to_sb/question/QuestionService.java
@@ -2,6 +2,9 @@ package com.ll.mission_jump_to_sb.question;
 
 import com.ll.mission_jump_to_sb.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -16,6 +19,11 @@ public class QuestionService {
 
     public List<Question> getList() {
         return this.questionRepository.findAll();
+    }
+
+    public Page<Question> getList(int page) {
+        Pageable pageable = PageRequest.of(page, 10);
+        return this.questionRepository.findAll(pageable);
     }
 
     public Question getQuestion(Integer id) {
