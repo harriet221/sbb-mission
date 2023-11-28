6e30b5919ba46a1abf53d6ba2cceba2f2d31dd0d
harriet221 <potter19@dongguk.edu>
2023-11-28 17:46:27 +0900

    chapter 3-06 회원가입 4

--- src/main/java/com/ll/mission_jump_to_sb/user/UserController.java
+++ src/main/java/com/ll/mission_jump_to_sb/user/UserController.java
@@ -2,6 +2,7 @@ package com.ll.mission_jump_to_sb.user;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -29,7 +30,22 @@ public class UserController {
             return "signup_form";
         }
 
-        userService.create(userCreateForm.getUsername(), userCreateForm.getEmail(), userCreateForm.getPassword());
+        try {
+            userService.create(
+                    userCreateForm.getUsername(),
+                    userCreateForm.getEmail(),
+                    userCreateForm.getPassword() );
+
+        } catch (DataIntegrityViolationException e) {
+            e.printStackTrace();
+            bindingResult.reject("signupFailed", "이미 등록된 사용자입니다.");
+            return "signup_form";
+        } catch (Exception e) {
+            e.printStackTrace();
+            bindingResult.reject("signupFailed", e.getMessage());
+            return "signup_form";
+        }
+
         return "redirect:/";
     }
 }
