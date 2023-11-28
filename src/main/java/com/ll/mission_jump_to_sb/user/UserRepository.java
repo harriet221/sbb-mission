40a7d8b6eb1c0b08e7d07719762d74f59274e4d2
harriet221 <potter19@dongguk.edu>
2023-11-28 21:53:27 +0900

    chapter 3-07 로그인과 로그아웃 2

--- src/main/java/com/ll/mission_jump_to_sb/user/UserRepository.java
+++ src/main/java/com/ll/mission_jump_to_sb/user/UserRepository.java
@@ -2,5 +2,8 @@ package com.ll.mission_jump_to_sb.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<SiteUser, Long> {
+    Optional<SiteUser> findByusername(String username);
 }
