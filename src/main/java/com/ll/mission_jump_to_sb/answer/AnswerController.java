a86ffe63fe8d2939c151e33a0a98fe333c85de67
harriet221 <potter19@dongguk.edu>
2023-11-29 09:49:16 +0900

    chapter 3-08 엔티티 변경 2

--- src/main/java/com/ll/mission_jump_to_sb/answer/AnswerController.java
+++ src/main/java/com/ll/mission_jump_to_sb/answer/AnswerController.java
@@ -2,6 +2,8 @@ package com.ll.mission_jump_to_sb.answer;
 
 import com.ll.mission_jump_to_sb.question.Question;
 import com.ll.mission_jump_to_sb.question.QuestionService;
+import com.ll.mission_jump_to_sb.user.SiteUser;
+import com.ll.mission_jump_to_sb.user.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.security.Principal;
+
 @RequestMapping("/answer")
 @RequiredArgsConstructor
 @Controller
@@ -18,17 +22,23 @@ public class AnswerController {
 
     private final QuestionService questionService;
     private final AnswerService answerService;
+    private final UserService userService;
 
     @PostMapping("/create/{id}")
-    public String createAnswer(Model model, @PathVariable("id") Integer id,
-                               @Valid AnswerForm answerForm, BindingResult bindingResult) {
+    public String createAnswer(Model model,
+                               @PathVariable("id") Integer id,
+                               @Valid AnswerForm answerForm,
+                               BindingResult bindingResult,
+                               Principal principal) {
 
         Question question = this.questionService.getQuestion(id);
+        SiteUser siteUser = this.userService.getUser(principal.getName());
+
         if(bindingResult.hasErrors()) {
             model.addAttribute("question", question);
             return "question_detail";
         }
-        this.answerService.create(question, answerForm.getContent());
+        this.answerService.create(question, answerForm.getContent(), siteUser);
         return String.format("redirect:/question/detail/%s", id);
     }
 }
--- src/main/java/com/ll/mission_jump_to_sb/answer/AnswerService.java
+++ src/main/java/com/ll/mission_jump_to_sb/answer/AnswerService.java
@@ -1,6 +1,7 @@
 package com.ll.mission_jump_to_sb.answer;
 
 import com.ll.mission_jump_to_sb.question.Question;
+import com.ll.mission_jump_to_sb.user.SiteUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,11 +13,12 @@ public class AnswerService {
 
     private final AnswerRepository answerRepository;
 
-    public void create(Question question, String content) {
+    public void create(Question question, String content, SiteUser author) {
         Answer answer = new Answer();
         answer.setContent(content);
         answer.setCreateDate(LocalDateTime.now());
         answer.setQuestion(question);
+        answer.setAuthor(author);
         this.answerRepository.save(answer);
     }
 }
